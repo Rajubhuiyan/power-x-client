2d55800eec4ee6df3cc756faf0d5d8ec2264ed92
rajubhuiyan <rajubhuiyaann@gmail.com>
2021-11-08 13:24:15 +0600

    complete

--- src/Components/Login/Login/firebase.config.js
+++ src/Components/Login/Login/firebase.config.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyC0vBDRHLW3kXeK7668PhXEcQwmiOZPx-o",
-  authDomain: "power-x-s.firebaseapp.com",
-  projectId: "power-x-s",
-  storageBucket: "power-x-s.appspot.com",
-  messagingSenderId: "761926232630",
-  appId: "1:761926232630:web:818a294fb0da1e0b538697"
+  apiKey: "AIzaSyBW5DWKlxYHPj-ZAgiswG5hIKqZszDds8I",
+  authDomain: "power-xgym.firebaseapp.com",
+  projectId: "power-xgym",
+  storageBucket: "power-xgym.appspot.com",
+  messagingSenderId: "631796276479",
+  appId: "1:631796276479:web:13cdfc93db8148c67ec0ca"
 };
 
   export default firebaseConfig;
